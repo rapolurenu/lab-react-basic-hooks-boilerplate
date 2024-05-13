3729a46aa4b89d474c4034199548efabfd5bc016
rapolurenu <159129847+rapolurenu@users.noreply.github.com>
2024-05-13 23:50:29 +0530

    Create UseContext.jsx

---
+++ basicHooks/src/components/UseContext.jsx
@@ -0,0 +1,7 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../App';
+
+function ThemeDisplay() {
+  const darkMode = useContext(ThemeContext);  
+  };
+export default ThemeDisplay;
